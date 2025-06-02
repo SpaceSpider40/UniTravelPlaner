35e34e34843d8055019fec1951f388d3f1c89147
SpaceSpider <spacespiderproductions@gmail.com>
2025-06-02 16:51:25 +0200

    Initial commit

---
+++ src/main/java/uni/unitravelplaner/UniTravelPlanerApplication.java
@@ -0,0 +1,13 @@
+package uni.unitravelplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniTravelPlanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniTravelPlanerApplication.class, args);
+	}
+
+}
